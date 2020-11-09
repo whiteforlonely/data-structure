40d1e654c5e197a32225a559cd3a6aed44771c53
Saturday <qingyuan_888930@sina.com>
2020-11-09 18:17:21 +0800

    add delete red-black-tree node

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # data-structure
 here as I want to express, just some data structure test and debug issue. about left rotate and right rotate, it is still need time to consume, here  is just finished the addNode, not still not test much. so, I want to use the rest time of this day to test the code and try to finish the  delete algorithm.
 
+delete algorithm need some reference.
 ## reference
 1. [red-black-tree visualization](https://www.cs.usfca.edu/~galles/visualization/RedBlack.html)
+2. [彻底理解红黑树（三）之 删除](https://www.jianshu.com/p/84416644c080)
--- datastructure/src/main/java/com/ake/structure/redblacktree/RedBlackTree.java
+++ datastructure/src/main/java/com/ake/structure/redblacktree/RedBlackTree.java
@@ -42,6 +42,10 @@ public class RedBlackTree {
                 }
             }
         }
+        if (node.getParent() == null) {
+            // 没有可以插入的地方，因为是重复的值，核心外的东西。
+            return;
+        }
         // 只要判断父节点和祖父节点情况
         // 判断父节点的颜色
         while (currNode != null && currNode.isRed() && node.isRed()) {
@@ -142,9 +146,241 @@ public class RedBlackTree {
         }
     }
 
+    // 获取兄弟节点
+    private TreeNode<Integer> getBrother(TreeNode<Integer> node) {
+        if (node == null) {
+            return null;
+        }
+        TreeNode<Integer> pNode = node.getParent();
+        if (pNode == null) {
+            return null;
+        }
+        if (pNode.getLeft() == node) {
+            return pNode.getRight();
+        }
+        return pNode.getLeft();
+    }
+
     // 删除节点
     public void removeNode(int value){
+        // 红黑树的删除涉及到的地方应该是有很多的
+        // 1. 找到对应节点
+        if (root == null) {
+            return;
+        }
+        TreeNode<Integer> currNode = root;
+        while (currNode != null && currNode.getValue() != value) {
+            if (value < currNode.getValue()) {
+                currNode = currNode.getLeft();
+            } else if (value > currNode.getValue()) {
+                currNode = currNode.getRight();
+            }
+        }
+        // 最后还是没有找到
+        if (currNode == null) {
+            System.err.println("can not find deleted node");
+        }
 
+        // 开始删除节点
+        TreeNode<Integer> removedNode = currNode;
+        // 二叉树 删除操作
+
+        // 模拟删除二叉树
+        while (removedNode != null) {
+
+            if (removedNode.getLeft() == null && removedNode.getRight() == null) {
+                if (!removedNode.isRed()) {
+                    // 删除的是黑色的节点，需要进行平衡
+                    removedNode = reblanceTree(removedNode);
+                }
+                // 删除的节点是红色的，并且是无子节点的
+                if ((removedNode.getParent().getLeft() == removedNode)) {
+                    removedNode.getParent().setLeft(null);
+                } else {
+                    removedNode.getParent().setRight(null);
+                }
+                removedNode.setParent(null);
+                removedNode = null;
+            } else if (removedNode.getLeft() != null && removedNode.getRight() == null) {
+                // 只有一个子节点
+                if ((removedNode.getParent().getLeft() == removedNode)) {
+                    removedNode.getParent().setLeft(removedNode.getLeft());
+                } else {
+                    removedNode.getParent().setRight(removedNode.getLeft());
+                }
+                removedNode.setParent(null);
+                removedNode = null;
+            } else if (removedNode.getRight() != null && removedNode.getLeft() == null) {
+                // 只有一个子节点
+                if ((removedNode.getParent().getLeft() == removedNode)) {
+                    removedNode.getParent().setLeft(removedNode.getRight());
+                } else {
+                    removedNode.getParent().setRight(removedNode.getRight());
+                }
+                removedNode.setParent(null);
+                removedNode = null;
+            } else if (removedNode.getRight() != null && removedNode.getLeft() != null) {
+                // 同时有两个子节点
+                TreeNode<Integer> nextRemovedNode = getNextReplaceNode(removedNode);
+                removedNode.setValue(nextRemovedNode.getValue());
+                removedNode = nextRemovedNode;
+            }
+        }
+
+        // END
+    }
+
+    // 获取后继节点
+    private TreeNode<Integer> getNextReplaceNode(TreeNode<Integer> removedNode){
+        if (removedNode == null) {
+            return null;
+        }
+        TreeNode<Integer> left = removedNode.getLeft();
+        TreeNode<Integer> right = removedNode.getRight();
+        if (left == null && right == null) {
+            return null;
+        }
+        if (left != null) {
+            if (left.getRight() != null) {
+                return left.getRight();
+            } else {
+                return left;
+            }
+        }
+        if (right.getLeft() != null) {
+            return right.getLeft();
+        }
+        return right;
+    }
+
+    // 删除时重新进行平衡
+    private TreeNode<Integer> reblanceTree(TreeNode<Integer> currNode){
+
+        TreeNode<Integer> removedNode = currNode;
+        // 重新平衡红黑树
+        while (currNode != null && currNode != root && !currNode.isRed()) {
+            if (currNode.getParent().getLeft() == currNode) {
+                // 兄弟为右孩子
+                TreeNode<Integer> bNode = currNode.getParent().getRight();
+                if (bNode.isRed()) {
+                    // 兄弟节点为红色
+                    bNode.setRed(false);
+                    bNode.getParent().setRed(true);
+                    leftTransfer(bNode.getParent(), currNode, bNode, bNode.getLeft(), bNode.getRight());
+                } else {
+                    // 兄弟节点为黑色
+                    if (bNode.getLeft() != null && !bNode.getLeft().isRed() && bNode.getRight() != null && !bNode.getRight().isRed()
+                            || bNode.getLeft() == null && bNode.getRight() == null) {
+                        bNode.setRed(true);
+                        // 根据父节点的颜色来做处理
+                        if (currNode.getParent().isRed()) {
+                            // 父亲节点是红色的时候
+                            currNode.getParent().setRed(false);
+                            break;
+                        } else {
+                            currNode = currNode.getParent();
+                        }
+                    } else if (bNode.getRight() !=  null && bNode.getRight().isRed()) {
+                        // 兄弟节点的右孩子是红色的
+                        // 交换颜色
+                        boolean parentRed = bNode.getParent().isRed();
+                        boolean bRed = bNode.isRed();
+                        bNode.getParent().setRed(bRed);
+                        bNode.setRed(parentRed);
+                        // 兄弟节点右孩子涂黑
+                        bNode.getRight().setRed(false);
+                        // 父节点 左旋
+                        leftTransfer(bNode.getParent(), currNode, bNode, bNode.getLeft(), bNode.getRight());
+                    } else if (bNode.getRight() == null || !bNode.getRight().isRed()) {
+                        // 兄弟的右孩子是黑色的
+                        if (bNode.getLeft() != null) {
+                            bNode.getLeft().setRed(false);
+                        }
+                        bNode.setRed(true);
+                        // 兄弟节点 左旋
+                        rightTransfer(bNode, bNode.getLeft(), bNode.getRight(), null, null);
+                    }
+                }
+            } else {
+                // 兄弟为左孩子
+                TreeNode<Integer> bNode = currNode.getParent().getLeft();
+                if (bNode.isRed()) {
+                    // 兄弟节点为红色
+                    bNode.setRed(false);
+                    bNode.getParent().setRed(true);
+                    rightTransfer(bNode.getParent(), bNode, currNode, bNode.getLeft(), bNode.getRight());
+                } else {
+                    // 兄弟节点为黑色
+                    if (bNode.getLeft() != null && !bNode.getLeft().isRed() && bNode.getRight() != null && !bNode.getRight().isRed()
+                    || bNode.getLeft() == null && bNode.getRight() == null) {
+                        bNode.setRed(true);
+                        // 根据父节点的颜色来做处理
+                        if (currNode.getParent().isRed()) {
+                            // 父亲节点是红色的时候
+                            currNode.getParent().setRed(false);
+                            break;
+                        } else {
+                            currNode = currNode.getParent();
+                        }
+                    } else if (bNode.getLeft() !=  null && bNode.getLeft().isRed()) {
+                        // 兄弟节点的左孩子是红色的
+                        // 交换颜色
+                        boolean parentRed = bNode.getParent().isRed();
+                        boolean bRed = bNode.isRed();
+                        bNode.getParent().setRed(bRed);
+                        bNode.setRed(parentRed);
+                        // 兄弟节点左孩子涂黑
+                        bNode.getLeft().setRed(false);
+                        // 父节点 右旋
+                        rightTransfer(bNode.getParent(), bNode, currNode, bNode.getLeft(), bNode.getRight());
+                    } else if (bNode.getLeft() == null || !bNode.getLeft().isRed()) {
+                        // 兄弟的左孩子是黑色的
+                        if (bNode.getRight() != null) {
+                            bNode.getRight().setRed(false);
+                        }
+                        bNode.setRed(true);
+                        // 兄弟节点 左旋
+                        leftTransfer(bNode, bNode.getLeft(), bNode.getRight(), null, null);
+                    }
+                }
+            }
+        }
+
+        return removedNode;
+    }
+
+    // 二叉树的删除
+    private TreeNode<Integer> bTreeDelete(TreeNode<Integer> removedNode) {
+        TreeNode<Integer> x,y;
+        if (removedNode.getLeft() == null || removedNode.getRight() == null) {
+            y = removedNode;
+        } else {
+            y = removedNode.getRight();
+        }
+        if (null != y.getLeft()) {
+            x = y.getLeft();
+        } else {
+            x = y.getRight();
+        }
+
+        x.setParent(y.getParent());
+
+        if (y.getParent() == null) {
+            root = x;
+        } else if (y.getParent().getLeft() == y) {
+            // 左孩子
+            y.getParent().setLeft(x);
+        } else {
+            y.getParent().setRight(x);
+        }
+
+        if (removedNode != y) {
+            removedNode.setValue(y.getValue());
+        }
+        if (!y.isRed()) {
+
+        }
+        return y;
     }
 
     // 左旋
@@ -229,12 +465,17 @@ public class RedBlackTree {
 
     public static void main(String[] args) {
         RedBlackTree tree = new RedBlackTree();
-        for (int i = 0; i < 12; i ++) {
+        for (int i = 0; i < 9; i ++) {
             tree.addNode(i);
             if (i == 7) {
                 System.out.println("test point");
             }
         }
+
+        tree.removeNode(3);
+        tree.removeNode(0);
+        tree.removeNode(1);
+        tree.removeNode(7);
 
         tree.print1(tree.root);
     }
